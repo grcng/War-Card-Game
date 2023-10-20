d5f0d974993bc6dde29a09eac7c09d79750cbcc2
Viri Nguyen <123845932+grcng@users.noreply.github.com>
2023-10-20 15:28:01 -0400

    Update main.rs

--- war/rust/war_rs/src/main.rs
+++ war/rust/war_rs/src/main.rs
@@ -1,11 +1,7 @@
 #![allow(non_snake_case,non_camel_case_types,dead_code)]
 
 /*
-    Below is the function stub for deal. Add as many helper functions
-    as you like, but the deal function should not be modified. Just
-    fill it in.
-    
-    Test your code by running 'cargo test' from the war_rs directory.
+    Test the code by running 'cargo test' from the war_rs directory.
 */
 
 fn deal(shuf: &[u8; 52]) -> [u8; 52] 
